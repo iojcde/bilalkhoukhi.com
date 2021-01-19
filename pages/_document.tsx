0220fa0e648c28d1e2bbdf6418ea6fa6dd9648d1
BK <hello@bilalkhoukhi.com>
2021-01-18 20:43:07 -0600

    Adds microsoft clarity tracking code - privacy oriented solution

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,8 +8,20 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
-        <Head />
+      <Html lang="en">
+        <Head>
+          <script 
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", ${process.env.NEXT_PUBLIC_MS_CLARITY});
+              `
+            }}
+          />
+        </Head>
         <body className="bg-white dark:bg-grey-900 text-grey-900 dark:text-white font-roboto-mono transition-colors duration-500">
           <Main />
           <NextScript />
