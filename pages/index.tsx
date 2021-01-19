7d52d7ed38052703cd964cf9779b7525e8518e20
BK <hello@bilalkhoukhi.com>
2021-01-18 20:43:27 -0600

    Updates the blog page to include a short intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,15 +9,32 @@ const Blog = ({ posts, prevPosts, nextPosts }) => {
   
   return (
     <Layout title="BK's Blog">
-        <h1 className="mb-8 text-4xl">Blog</h1>
-        {posts &&
-        posts
-          .filter((post) => {
-            return isLocal || !post.draft
-          })
-          .map((post) => (
-            <PostCard key={post.slug} post={post}/>
-          ))}
+      <div className="">
+        {/* <!-- Page header --> */}
+        <h1 className="mb-1 text-4xl">Hi, I am BK</h1>
+        {/* <!-- Page sub-header --> */}
+        <p className="text-lg">Front-End Engineer @ BuildOn Technologies, located in HTX</p>
+        
+        <Link href="/about">
+        {/* w-full sm:w-auto flex-none bg-gray-900 hover:bg-gray-700 text-white text-lg leading-6 font-semibold py-3 px-6  */}
+          <a className="bg-red-500 hover:bg-red-400 text-white inline-flex align-center w-auto py-2 px-4 mt-4 mb-8 group hover:pr-2 border border-red-200 border-transparent rounded-md focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-500 focus:ring-gray-900 focus:outline-none transition-colors duration-200">
+            <span className="text-md leading-7">Learn more</span>
+            <svg className="h-8 ml-4 group-hover:ml-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+            </svg>
+          </a>
+        </Link>
+
+      </div>
+      <h1 className="mb-8 text-4xl">Blog</h1>
+      {
+        posts && posts.filter((post) => {
+          return isLocal || !post.draft
+        })
+        .map((post) => (
+          <PostCard key={post.slug} post={post}/>
+        ))
+      }
       <div>
         <div>
           {prevPosts !== null && (
